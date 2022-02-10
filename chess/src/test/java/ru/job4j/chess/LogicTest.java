74290565df835400110b92fc1dba8a8469736853
denvoiten <den.voiten@gmail.com>
2022-02-10 14:02:11 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,15 +4,40 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
 public class LogicTest {
 
-    @Ignore
     @Test
     public void move()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void moveFigureNotFound()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F8));
+        logic.move(Cell.F7, Cell.A4);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void moveTroughPawnBlackFail()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F8));
+        logic.add((new PawnBlack(Cell.E7)));
+        logic.move(Cell.F8, Cell.D6);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void moveNotDiagonalBishopFail()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.F8));
+        logic.move(Cell.F8, Cell.F6);
     }
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class BishopBlackTest {
+
+    @Test
+    public void position() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        BishopBlack expected = new BishopBlack(Cell.C8);
+        assertThat(bishopBlack.position(), is(expected.position()));
+    }
+
+    @Test
+    public void copy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        BishopBlack expected = new BishopBlack(Cell.B7);
+        assertThat(bishopBlack.copy(Cell.B7).position(), is(expected.position()));
+    }
+
+    @Ignore
+    @Test
+    public void way() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        assertThat(bishopBlack.way(Cell.G5), is(new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5}));
+    }
+}
