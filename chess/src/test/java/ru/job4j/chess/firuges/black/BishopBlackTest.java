e011d6b230ce78ab36bbbb0b6cecf8f95d68b29b
denvoiten <den.voiten@gmail.com>
2022-05-29 17:35:38 +0300

    Moved Test

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -9,6 +9,12 @@ import static org.hamcrest.Matchers.is;
 public class BishopBlackTest {
 
     @Test
+    public void way() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        assertThat(bishopBlack.way(Cell.G5), is(new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5}));
+    }
+
+    @Test
     public void position() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C8);
         BishopBlack expected = new BishopBlack(Cell.C8);
@@ -20,11 +26,5 @@ public class BishopBlackTest {
         BishopBlack bishopBlack = new BishopBlack(Cell.C8);
         BishopBlack expected = new BishopBlack(Cell.B7);
         assertThat(bishopBlack.copy(Cell.B7).position(), is(expected.position()));
-    }
-
-    @Test
-    public void way() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        assertThat(bishopBlack.way(Cell.G5), is(new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5}));
     }
 }
